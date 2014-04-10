5a1cb0517fc2e927f51a23054cab02dff5fd0da3
Sasha Nicolas <sasha@laws.deinf.ufma.br>
2014-04-10 18:23:42 -0300

    fixing bugs

--- Gamut/main.cpp
+++ Gamut/main.cpp
@@ -5,23 +5,25 @@
 #include <time.h>
 #include "color.h"
 
-float k;
+#define nPoints 10000
 
-// Initialization function
-static void Init(){
-    float white[4] = {1.0f,1.0f,1.0f,1.0f};
-    glClearColor(0, 0, 0, 1.0f);
-    //glEnable(GL_DEPTH_TEST);
-    
-    //glEnable(GL_LIGHTING);
-    //glEnable(GL_LIGHT0);
-    //glEnable(GL_COLOR_MATERIAL);
-    //glEnable(GL_NORMALIZE);
-    //glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
-    //glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,white);
-    //glMaterialf(GL_FRONT_AND_BACK,GL_SHININESS,50.0);
-    //glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER,1);
-}
+float k;
+float points[nPoints][3];
+
+// Camera position
+float x = -2.5, z = 2; // initially 5 units south of origin
+float deltaMove = 0.0; // initially camera doesn't move
+
+// Camera direction
+float lx = 0.0, lz = 0.0; // camera points initially along y-axis
+float angle = 0.0; // angle of rotation for the camera direction
+float deltaAngle = 0.0; // additional angle change when dragging
+
+// Mouse drag control
+int isDragging = 0; // true when dragging
+int xDragStart = 0; // records the x-coordinate when dragging starts
+
+
 
 // Draw scene
 static void DrawAxes(){
@@ -89,7 +91,7 @@ void getColorXYZ(float *X, float *Y, float *Z){
     float beta[401];
     for (int i=0; i<=400; i++) {
         beta[i] = ( rand() % 100 ) / 100.0;
-        printf("%f;\n",beta[i]);
+        //printf("%f;\n",beta[i]);
     }
     
     float x_bar, y_bar, z_bar, L;
@@ -116,26 +118,13 @@ static void DrawScene(){
 //    float lpos[4] = {0.0f,100.0f,0.0f,1.0f};
     //glLightfv(GL_LIGHT0,GL_POSITION,lpos);
 
-    float X, Y, Z;
-    float r, g, b;
-
     //desenhar cubo limitante
     DrawCube();
-    
-    int n = 0;
-    calculateK(&k);
 
     glBegin(GL_POINTS);
-    while (n<1) {
-        getColorXYZ( &X, &Y, &Z);
-        corCIEXYZtoCIERGB(X,Y,Z, &r, &g, &b);
-        
-        //printf("%f; %f; %f;\n",r,g,b);
-        
-        glColor3f(r, g, b);
-        glVertex3f(r, g, b);
-        
-        n++;
+    for (int i=0; i<nPoints; i++) {
+        glColor3f(points[i][0], points[i][1], points[i][2]);
+        glVertex3f(points[i][0], points[i][1], points[i][2]);
     }
     glEnd();
     
@@ -154,7 +143,7 @@ static void Display (void){
     glLoadIdentity();
     
     // load manipulator matrix
-    gluLookAt(-2.5,1.5,2,
+    gluLookAt(x + lx, 1.5, z + lz,
               0.5,0.5,0.5,
               0,1,0);
     
@@ -176,6 +165,91 @@ static void Keyboard (unsigned char key, int x, int y){
     }
 }
 
+
+// Initialization function
+static void Init(){
+    float white[4] = {1.0f,1.0f,1.0f,1.0f};
+    glClearColor(0, 0, 0, 1.0f);
+    //glEnable(GL_DEPTH_TEST);
+    
+    //glEnable(GL_LIGHTING);
+    //glEnable(GL_LIGHT0);
+    //glEnable(GL_COLOR_MATERIAL);
+    //glEnable(GL_NORMALIZE);
+    //glColorMaterial(GL_FRONT_AND_BACK,GL_AMBIENT_AND_DIFFUSE);
+    //glMaterialfv(GL_FRONT_AND_BACK,GL_SPECULAR,white);
+    //glMaterialf(GL_FRONT_AND_BACK,GL_SHININESS,50.0);
+    //glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER,1);
+    
+    
+    float X, Y, Z;
+    float r, g, b;
+    
+    int n = 0;
+    calculateK(&k);
+
+    while (n<nPoints) {
+        getColorXYZ( &X, &Y, &Z);
+        corCIEXYZtoCIERGB(X,Y,Z, &r, &g, &b);
+        
+        points[n][0] = r;
+        points[n][1] = g;
+        points[n][2] = b;
+        
+        n++;
+    }
+}
+
+//----------------------------------------------------------------------
+// Update with each idle event
+//
+// This incrementally moves the camera and requests that the scene be
+// redrawn.
+//----------------------------------------------------------------------
+void update(void)
+{
+	if (deltaMove) { // update camera position
+		x += deltaMove * lx * 0.1;
+		z += deltaMove * lz * 0.1;
+	}
+	glutPostRedisplay(); // redisplay everything
+}
+
+//----------------------------------------------------------------------
+// Process mouse drag events
+//
+// This is called when dragging motion occurs. The variable
+// angle stores the camera angle at the instance when dragging
+// started, and deltaAngle is a additional angle based on the
+// mouse movement since dragging started.
+//----------------------------------------------------------------------
+void mouseMove(int x, int y)
+{
+	if (isDragging) { // only when dragging
+		// update the change in angle
+		deltaAngle = (x - xDragStart) * 0.005;
+        
+		// camera's direction is set to angle + deltaAngle
+		lx = -sin(angle + deltaAngle);
+		lz = cos(angle + deltaAngle);
+	}
+}
+
+void mouseButton(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON) {
+		if (state == GLUT_DOWN) { // left mouse button pressed
+			isDragging = 1; // start dragging
+			xDragStart = x; // save x where button first pressed
+		}
+		else  { /* (state = GLUT_UP) */
+			angle += deltaAngle; // update camera turning angle
+			isDragging = 0; // no longer dragging
+		}
+	}
+}
+
+
 // Main function
 int main (int argc, char* argv[]){
     srand(time(NULL));
@@ -189,6 +263,10 @@ int main (int argc, char* argv[]){
     glutReshapeFunc(Reshape);
     glutDisplayFunc(Display);
     glutKeyboardFunc(Keyboard);
+    glutMouseFunc(mouseButton); // process mouse button push/release
+	glutMotionFunc(mouseMove); // process mouse dragging motion
+    glutIdleFunc(update); // incremental update
+	
     
     // initiate OpenGL context
     Init();
